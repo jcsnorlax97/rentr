d462813ca5e2412c8d8b344995fc4fa7bd348d97
Mik3Qiao <mike0524745@gmail.com>
2021-02-17 15:20:59 -0600

    Added some sample, tested  mock rest with soapui

--- frontend/src/actions/ListingDetail.js
+++ frontend/src/actions/ListingDetail.js
@@ -1,41 +1,6 @@
-export const setPrice = (inputInt) => {
+export const setListingArray = (inputArr) => {
   return {
-    type: "SET_PRICE",
-    payload: inputInt
-  }
-}
-
-export const setBedRoom = (inputInt) => {
-  return {
-    type: "SET_BEDROOM",
-    payload: inputInt
-  }
-}
-
-export const setWashRoom = (inputInt) => {
-  return {
-    type: "SET_WASHROOM",
-    payload: inputInt
-  }
-}
-
-export const setLaundryRoom = (inputInt) => {
-  return {
-    type: "SET_LAUNDRYROOM",
-    payload: inputInt
-  }
-}
-
-export const setDescription = (inputString) =>{
-  return {
-    type: "SET_DESCRIPTION",
-    payload: inputString
-  }
-}
-
-export const setImage = (inputString) =>{
-  return {
-    type: "SET_IMAGE",
-    payload: inputString
+    type: "SET_LISTING_ARRAY",
+    payload: inputArr
   }
 }
--- frontend/src/components/Listing/listing.jsx
+++ frontend/src/components/Listing/listing.jsx
@@ -2,31 +2,36 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {
-  setPrice,
-  setBedRoom,
-  setWashRoom,
-  setLaundryRoom,
-  setDescription,
-  setImage
+  setListingArray
 } from "../../actions/ListingDetail";
 import apartment1 from "../../resources/apartment1.jpg";
 import axios from "axios";
+import Paper from '@material-ui/core/Paper';
+import BathtubIcon from '@material-ui/icons/Bathtub';
+import HotelIcon from '@material-ui/icons/Hotel';
+import LocalLaundryServiceIcon from '@material-ui/icons/LocalLaundryService';
+import PetsIcon from '@material-ui/icons/Pets';
+import Divider from '@material-ui/core/Divider';
+import { Typography } from "@material-ui/core";
+
+import "../../styles/Listing.css";
+
 class Listing extends Component {
-  state = {
-    imageSource: ""
-  }
 
   componentDidMount (){
-    // this.props.setBedRoom(2);
-    // this.props.setWashRoom(10);
     // axios.get("http://localhost:3005/apartment1")
     // .then(response =>{
     //   this.setState({
     //     imageSource: response.data[0].source
     //   })
     // })
-    this.setState({
-      imageSource: this.getEmergencyFoundImg(apartment1)
+
+    // this.setState({
+    //   imageSource: this.getEmergencyFoundImg(apartment1)
+    // })
+    axios.get("http://localhost:3005/listingArray")
+    .then(response =>{
+      this.props.setListingArray(response.data)
     })
   }
 
@@ -46,24 +51,135 @@ class Listing extends Component {
     return b64;
   };
 
-  handleClick = () => {
-    console.log("image source is: " + this.state.imageSource)
-  }
-
   render() {
     return (
       <div
-        className="listing"
+        className="listingContent"
       >
-        {/* {this.props.washRoom} */}
-        <img
+        {/* {this.props.listingArray.map(listingDetail, index) => (
+
+        )} */}
+        {this.props.listingArray && this.props.listingArray.length !== 0 
+        ? 
+          this.props.listingArray.map((listingDetail, index)=>{
+          // return
+          return (
+            <Paper
+              key = {index}
+              elevation = {3}
+              style = {{
+                width: "90%",
+                minWidth: 600,
+                height: 150,
+                margin: 16
+              }}
+              className = "individualListingContent"
+            >
+              {/* This is for the image area */}
+              <span className = "listingImageArea">
+                <img
+                  style = {{
+                    width: "100%",
+                    height: "100%"
+                  }}
+                  // src={`data:image/png;base64,${this.state.imageSource}`}
+                  src={apartment1}
+                  alt="apartment"
+                />
+              </span>
+
+              <div className = "listingTextAndIcon">
+                <span className = "listingHeader">
+                
+                  {/* This is for the listing title area */}
+                  <span className = "listingTitle">
+                    {listingDetail.title}
+                  </span>
+
+                  <Typography
+                    type="title"
+                    color="inherit" 
+                    style={{
+                      flex: 1 
+                    }}
+                  />
+            
+                  {/* This is for the listing icon area */}
+                  <span className = "listingIconGroup">
+                    {/* number of washrooms*/}
+                    <span className = "listingIconNumber">
+                      {listingDetail.washroom}
+                      <BathtubIcon className = "listingIcon" fontSize = "large"/>
+                    </span>
+
+                    {/* Number of bedrooms */}
+                    <span className = "listingIconNumber">
+                      {listingDetail.bedroom}
+                      <HotelIcon className = "listingIcon" fontSize = "large"/>
+                    </span>
+                    
+                    {/* Number of laundry rooms */}
+                    <span className = "listingIconNumber">
+                      {listingDetail.laundryroom}
+                      <LocalLaundryServiceIcon className = "listingIcon" fontSize = "large"/>
+                    </span>
+                    
+                    {/* Indicate whether pets allowed or not */}
+                    <span>
+                      <PetsIcon
+                        style = {
+                          listingDetail.pet 
+                          ? {color: "green"} // if allowed, show color as green
+                          : {color: "grey"}  // else, show color as grey
+                        }
+                        className = "listingIconNumber"
+                      />
+                    </span>
+
+                  </span>
+                  
+                  <Divider orientation="vertical" flexItem />
+
+                  <span
+                    className = "listingPrice"
+                  >
+                    ${listingDetail.price}
+                  </span>
+
+                </span>
+
+                <Divider/>
+
+                <div className = "listingDescription">
+                  {listingDetail.description}
+                </div>
+
+              </div>
+            </Paper>
+          )})
+        : 
+          <Paper
+            elevation = {3}
+            style = {{
+              width: "auto",
+              minWidth: 600,
+              height: 150,
+              margin: 20
+            }}
+          >
+            <h2>
+              No listing information at the moment
+            </h2>
+          </Paper>
+        }
+        {/* <img
           style = {{
             width: "100%",
             height: "100%"
           }}
-          src={`data:image/png;base64,${this.state.imageSource}`}
+          // src={`data:image/png;base64,${this.state.imageSource}`}
           alt="apartment 1"
-        />
+        /> */}
       </div>
     )
   }
@@ -72,23 +188,13 @@ class Listing extends Component {
 //REDUX
 const mapStateToProps = state => {
   return {
-    price: state.listingDetail.price,
-    bedRoom: state.listingDetail.bedRoom,
-    washRoom: state.listingDetail.washRoom,
-    laundryRoom: state.listingDetail.laundryRoom,
-    description: state.listingDetail.description,
-    imageSource: state.listingDetail.imageSource,
+    listingArray: state.listingDetail.listingArray,
   };
 };
 
 const matchDispatchToProps = dispatch => {
   return bindActionCreators({
-    setPrice,
-    setBedRoom,
-    setWashRoom,
-    setLaundryRoom,
-    setDescription,
-    setImage
+    setListingArray,
   }, dispatch);
 };
 export default connect(mapStateToProps, matchDispatchToProps)(Listing);
