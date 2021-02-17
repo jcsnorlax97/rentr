cddaf3526f76b9f86a32522155512ed6b4a0a95c
Justin <justin.choi.developer@gmail.com>
2021-02-16 21:29:16 -0600

    Issue #67: Update routes

---
+++ server/api/routes/listing.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { container } = require('../../di-setup');
+const validate = require('../../middleware/validate');
+const listingDto = require('../../dto/listing');
+
+// --- get classes via container ---
+const listingController = container.resolve('listingController');
+
+const router = express.Router();
+router.post('/', validate(listingDto), listingController.createUser);
+
+module.exports = router;
