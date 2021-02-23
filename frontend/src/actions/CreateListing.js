b0f67a43a3d4d3774171131cddcc1c02447cea8d
NathanCarriere <carrie34@myumanitoba.ca>
2021-02-23 16:02:46 -0600

    Basic UI of Creating Listing Form
    Just the basic, barebones UI. This is without much design consideration... styling will be left for the future.

--- frontend/src/actions/CreateListing.js
+++ frontend/src/actions/CreateListing.js
@@ -5,6 +5,27 @@ export const setTitle = (inputString) => {
     }
 }
 
+export const setDescription = (inputString) => {
+    return {
+        type: "SET_DESCRIPTION",
+        payload: inputString
+    }
+}
+
+export const setNumberOfBedrooms = (inputString) => {
+    return {
+        type: "SET_NUMBER_OF_BEDROOMS",
+        payload: inputString
+    }
+}
+
+export const setNumberOfBathrooms = (inputString) => {
+    return {
+        type: "SET_NUMBER_OF_BATHROOMS",
+        payload: inputString
+    }
+}
+
 export const setDialogOpen = (inputBool) => {
     return {
         type: "SET_DIALOG_OPEN",
--- frontend/src/reducers/CreateListing.js
+++ frontend/src/reducers/CreateListing.js
@@ -1,5 +1,8 @@
 const initialState = {
     title: "",
+    description: "",
+    numberOfBedrooms: "",
+    numberOfBathrooms: "",
     dialogOpen: false
 }
 
@@ -7,6 +10,12 @@ export const createListingReducer = (state = initialState, action) => {
     switch (action.type) {
         case "SET_TITLE":
             return { ...state, title: action.payload };
+        case "SET_DESCRIPTION":
+            return { ...state, description: action.payload };
+        case "SET_NUMBER_OF_BEDROOMS":
+            return { ...state, numberOfBedrooms: action.payload };
+        case "SET_NUMBER_OF_BATHROOMS":
+            return { ...state, numberOfBathrooms: action.payload };
         case "SET_DIALOG_OPEN":
             return { ...state, dialogOpen: action.payload };
         default:
