1af5b756b6732ceb504d2becd483c68c396a34cd
AzizulHakim96 <56322456+AzizulHakim96@users.noreply.github.com>
2021-02-22 20:23:53 -0600

    wrote one test case

--- server/services/listing.test.js
+++ server/services/listing.test.js
@@ -57,4 +57,25 @@ describe('listingService', () => {
       expect(listings[0].title).toEqual("Expensive one");
     });
   });
+
+  describe('getOneListings test', () => {
+    //GIVEN
+    const listingDao = {
+      getOneListing: (id) => [{ "id": 2,
+      "title": "Expensive one",
+      "description": "one million dollars",
+      "num_bedroom": ">10",
+      "num_bathroom": "5"}]
+    };
+    const listingService = new ListingService({ listingDao });
+    
+    it('succeeds', () => {
+      // WHEN
+      const listings = listingService.getOneListing();
+
+      //THEN
+      //when the listing is in the database
+      expect(listings[0].id).toEqual(2);
+    });
+  });
 });
