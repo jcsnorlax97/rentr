2ab6b57c19ad10fa315d0f5adfc7ea31e9b22bb3
Justin <justin.choi.developer@gmail.com>
2021-02-24 01:45:28 -0600

    Ensure the entered password must be less than 100 characters

--- server/dto/user.js
+++ server/dto/user.js
@@ -2,7 +2,7 @@ const joi = require('@hapi/joi');
 
 const schema = joi.object({
   email: joi.string().email().required(),
-  password: joi.string().required(),
+  password: joi.string().required().max(100),
 });
 
 module.exports = schema;
