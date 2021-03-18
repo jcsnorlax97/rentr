fdb59d87bedf7a23f5da094d72ae83ed86f2ed74
Justin <justin.choi.developer@gmail.com>
2021-03-18 14:06:02 -0500

    Issue #140: Update get all listing route by calling validateReqQueryString(...) middleware before passing to the controller

--- server/api/routes/listing.js
+++ server/api/routes/listing.js
@@ -2,13 +2,19 @@ const express = require('express');
 const { container } = require('../../di-setup');
 const validate = require('../../middleware/validate');
 const validateLoggedIn = require('../../middleware/validateLoggedIn');
+const validateReqQueryString = require('../../middleware/validateReqQueryString');
 const listingDto = require('../../dto/listing');
+const listingReqQueryStringDto = require('../../dto/listingReqQueryString');
 
 // --- get classes via container ---
 const listingController = container.resolve('listingController');
 
 const router = express.Router();
-router.get('/', listingController.getAllListings);
+router.get(
+  '/',
+  validateReqQueryString(listingReqQueryStringDto),
+  listingController.getAllListings
+);
 router.get('/:id', listingController.getListingViaId);
 router.post(
   '/',
