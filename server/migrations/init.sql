6f9d06a7a9537ab2a928edf8ba988c88e6b28e99
Justin <justin.choi.developer@gmail.com>
2021-03-24 23:18:56 -0500

    Issue #136: Update SQL script for chain & comment tables

--- server/migrations/init.sql
+++ server/migrations/init.sql
@@ -8,6 +8,8 @@ CREATE DATABASE rentr;
 
 CREATE EXTENSION citext;
 
+DROP TABLE rentr_comment;
+DROP TABLE rentr_chain;
 DROP TABLE rentr_listing;
 DROP TABLE rentr_user;
 
@@ -47,3 +49,42 @@ INSERT INTO rentr_listing(userid, title, price, num_bedroom, num_bathroom, is_la
 (2, 'Serious appartment', 320, 3, 2, TRUE, TRUE, FALSE, ARRAY['this_is_the_image_url.com'], 'An appartment closed to the university and the bus stop. Contact me!');
 
 SELECT * FROM rentr_listing;
+
+CREATE TABLE IF NOT EXISTS rentr_chain (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    userid BIGINT NOT NULL, 
+    listingid BIGINT NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (userid) REFERENCES rentr_user(id),
+    CONSTRAINT fk_listing FOREIGN KEY (listingid) REFERENCES rentr_listing(id)
+);
+
+CREATE TABLE IF NOT EXISTS rentr_comment (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    userid BIGINT NOT NULL,
+    listingid BIGINT NOT NULL,
+    chainid BIGINT NOT NULL,
+    content VARCHAR CHECK (length(content) <= 1000) NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (userid) REFERENCES rentr_user(id),
+    CONSTRAINT fk_listing FOREIGN KEY (listingid) REFERENCES rentr_listing(id),
+    CONSTRAINT fk_chain FOREIGN KEY (chainid) REFERENCES rentr_chain(id)
+);
+
+INSERT INTO rentr_chain(userid, listingid) VALUES
+(3, 1),
+(2, 1),
+(4, 2);
+
+INSERT INTO rentr_comment(userid, listingid, chainid, content) VALUES
+(3, 1, 1, 'Tenant (uid=3) | Comment 1 in Chain 1 in Listing 1'),
+(1, 1, 1, 'Landlord (uid=1) | Comment 2 in Chain 1 in Listing 1'),
+(3, 1, 1, 'Tenant (uid=3) | Comment 3 in Chain 1 in Listing 1'),
+(2, 1, 1, 'Another Tenant (uid=2) | Comment 4 in Chain 1 in Listing 1'),
+(2, 1, 2, 'Tenant (uid=2) | Comment 5 in Chain 2 in Listing 1'),
+(1, 1, 2, 'Landlord (uid=1) | Comment 6 in Chain 2 in Listing 1'),
+(1, 1, 2, 'Landlord (uid=1) | Comment 7 in Chain 2 in Listing 1'),
+(1, 1, 2, 'Landlord (uid=1) | Comment 8 in Chain 2 in Listing 1'),
+(4, 2, 3, 'Tenant (uid=4) | Comment 9 in Chain 3 in Listing 2');
+
+SELECT * FROM rentr_chain;
+
+SELECT * FROM rentr_comment;
