ec1fdd8ccf288fb8b50214212e6ffccc56b240dd
Ronnie Dsouza <ron99ds@gmail.com>
2021-03-26 14:21:40 -0500

    Added Search for Single and Multiple results

--- server/cypress/integration/Search_spec.js
+++ server/cypress/integration/Search_spec.js
@@ -1,4 +1,25 @@
-describe('Simple Search', function(){
+describe('Simple Single Result Search', function(){
+    it('Find Search bar', function(){
+        cy.visit('https://rentr-front-end.herokuapp.com/')
+        cy.get('[id="headerSearchField"]')
+            .click()
+            .type('Super')
+            .should('have.value', 'Super')
+    })
+    it('Click Search Button', function(){
+        cy.get('[class="MuiButton-label"]')
+            .contains('Search')
+            .click()
+        
+    })
+    it('Validate Search Results', function(){
+        cy.get('[class="listingTitle"]')
+            .should('contain', 'Super Secret Testing Listing')
+    })
+
+})
+
+describe('Simple Multiple Results Search', function(){
     it('Find Search bar', function(){
         cy.visit('https://rentr-front-end.herokuapp.com/')
         cy.get('[id="headerSearchField"]')
@@ -11,6 +32,11 @@ describe('Simple Search', function(){
             .contains('Search')
             .click()
         
+    })
+    it('Validate Search Results', function(){
+        cy.get('[class="listingTitle"]')
+            .should('contain', ['Justin\'s Smoke Test Listing'])
+            .should('contain', ['Justin Test 2'])
     })
 
 })
