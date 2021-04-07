aab97e917ba5de984eda7311c5d0c229ca56feac
NathanCarriere <carrie34@myumanitoba.ca>
2021-04-07 12:53:03 -0500

    Add file for advanced search
    This adds a file for advanced search. Currently it doesn't do anything.

---
+++ server/e2e_tests/advancedSearchTest.js
@@ -0,0 +1,13 @@
+
+import {Selector} from 'testcafe';
+
+fixture('Search and advanced search test (no login))')
+    .page('https://rentr-front-end.herokuapp.com/');
+
+test('Search Test', async t => {
+    await t
+        // empty as of now. 
+
+        .wait(500)
+
+})
