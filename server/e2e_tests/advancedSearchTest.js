0a79676490f571784d5e307bc6230a237d18779c
NathanCarriere <carrie34@myumanitoba.ca>
2021-04-08 19:08:45 -0500

    Add ability to read mini-listings, split up search and advanced search
    This adds in functionality to read the contents of mini listings. Also, advanced search and search have been split up.

--- server/e2e_tests/advancedSearchTest.js
+++ server/e2e_tests/advancedSearchTest.js
@@ -4,29 +4,6 @@ import {Selector} from 'testcafe';
 fixture('Search and advanced search test (no login))')
     .page('https://rentr-front-end.herokuapp.com/');
 
-test('Search Test 1: Dalhousie Search Term', async t => {
-
-    await t
-        // Assuming there is a listing with the text 'Dalhousie' in the title, check if search 
-        // pulls up at least one listing with 'Apartment' in the title. 
-        .typeText('#headerSearchField', 'Dalhousie')
-        .click('#homePage_Header > div > span > button.MuiButtonBase-root.MuiButton-root.MuiButton-text.keywordSearchButton > span.MuiButton-label')
-        .wait(100)
-        .expect(Selector('#root > div > div > div > div > div.leftPanel-noDetails > div.MuiPaper-root.individualListingContent.MuiPaper-elevation3.MuiPaper-rounded > div > div.listingHeader > div.listingTitle').textContent).contains('Dalhousie', 'Should say Dalhousie')
-        .wait(500)
-})
-
-test('Search Test 2: Apartment Search Term', async t => {
-
-    await t
-        // Assuming there is a listing with the text 'Apartment' in the title, check if search 
-        // pulls up at least one listing with 'Apartment' in the title. 
-        .typeText('#headerSearchField', 'Apartment')
-        .click('#homePage_Header > div > span > button.MuiButtonBase-root.MuiButton-root.MuiButton-text.keywordSearchButton > span.MuiButton-label')
-        .wait(100)
-        .expect(Selector('#root > div > div > div > div > div.leftPanel-noDetails > div.MuiPaper-root.individualListingContent.MuiPaper-elevation3.MuiPaper-rounded > div > div.listingHeader > div.listingTitle').textContent).contains('Apartment', 'Should say Dalhousie')
-        .wait(500)
-})
 
 test('Advanced Search Test 1: copy info from first listing', async t => {
 
@@ -34,16 +11,22 @@ test('Advanced Search Test 1: copy info from first listing', async t => {
     // note: we don't really care about listing title, because that's not part of the
     // advanced search criteria
 
-    const bathrooms;
-    const bedrooms;
-    const laundry;
-    const pets;
-    const parking;
-    const price;
+    // aside: note that the mini-listing selectors will change based on whether a listing full view is open or not. 
+    const trueColor = 'rgb(0, 128, 0)';
+    const falseColor = 'dang';
+
+    const bathrooms = Selector('#root > div > div > div > div > div.leftPanel-noDetails > div:nth-child(1) > div > div.listingHeader > span > span:nth-child(1)').textContent;
+    const bedrooms = Selector('#root > div > div > div > div > div.leftPanel-noDetails > div:nth-child(1) > div > div.listingHeader > span > span:nth-child(2)').textContent;
+    const laundry = (trueColor === await (Selector('#root > div > div > div > div > div.leftPanel-noDetails > div:nth-child(2) > div > div.listingHeader > span > span:nth-child(3) > svg').getStyleProperty('color')));
+    const pets = (trueColor === await (Selector('#root > div > div > div > div > div.leftPanel-noDetails > div:nth-child(2) > div > div.listingHeader > span > span:nth-child(4) > svg').getStyleProperty('color')));
+    const parking = (trueColor === await (Selector('#root > div > div > div > div > div.leftPanel-noDetails > div:nth-child(2) > div > div.listingHeader > span > span:nth-child(5) > svg').getStyleProperty('color')));
+    const price = (await Selector('#root > div > div > div > div > div.leftPanel-noDetails > div:nth-child(2) > div > div.listingHeader > div.listingPrice').textContent).replace( /^\D+/g, '');
+    
+    console.log('info read from second listing: ', await bathrooms, await bedrooms, await laundry, await pets, await parking, await price)
 
     await t
-        // input the information from the first listing and see if it shows up
-        // (or some other listing that matches our search criteria)
+        
+        // input information gathered into advanced search
         .click('#homePage_Header > div > span > button.MuiButtonBase-root.MuiIconButton-root.advancedSearch-Button.MuiIconButton-edgeEnd > span.MuiIconButton-label > svg')
     
 })
