ead94dea59802acf56d130a7274c9bb677a77888
Ronnie Dsouza <ron99ds@gmail.com>
2021-04-09 13:21:16 -0500

    User Account Acceptance Tests
    Login Test
    Registration Test - Incomplete

---
+++ server/e2e_tests/loginTest.js
@@ -0,0 +1,17 @@
+import {Selector} from 'testcafe';
+
+fixture('Rentr Valid Acc Login')
+    .page('https://rentr-front-end.herokuapp.com/');
+
+test('Login Successfully', async t => {
+    await t
+        .maximizeWindow()
+        .click('[id="homePage_Header_Login"]')
+        .typeText('[id="loginEmail"]', 'demo1@gmail.com')
+        .typeText('[id="loginPassword"]', 'demoing')
+        .click('#loginDialog > div.MuiDialog-container.MuiDialog-scrollPaper > div > div.MuiDialogContent-root.homeDialog-Content > form > div.MuiDialogActions-root.homeDialog-Actions.MuiDialogActions-spacing > button.MuiButtonBase-root.MuiButton-root.MuiButton-text.homeDialog-normalButton > span.MuiButton-label')
+        .expect(Selector('div > div > div.MuiAlert-message').textContent).contains('Welcome', Selector('div > div > div.MuiAlert-message').textContent)
+        .wait(6000)
+
+        .expect(Selector('#homePage_Header > div > button > span.MuiButton-label').textContent).contains('Good', 'Missing Profile Section')
+})
